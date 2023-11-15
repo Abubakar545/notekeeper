4dd2b906c1f59ae2c460d062af56c716a023f75d
Syed <syed@git.com>
2023-11-15 12:33:15 +0530

    final commit

---
+++ build/precache-manifest.739cb18c827d63f43abd15d0623838d7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53bbecd6e2c14adf470312399334f03b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1470aaf4058d50b7ad6",
+    "url": "/static/js/2.4f973e14.chunk.js"
+  },
+  {
+    "revision": "564c1ab252e5111b308f",
+    "url": "/static/js/main.c46a3844.chunk.js"
+  },
+  {
+    "revision": "f7692ec575367ccd5afb",
+    "url": "/static/js/runtime-main.02a5c73c.js"
+  }
+]);
